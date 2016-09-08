801f82b85877f287e2a446e7102a5af4463ce99f
Santiago Toledano <stoledano@autentia.com>
2016-09-08 11:25:31 +0200

    Se elimina la funcionalidad de enviar por email los cambios realizados sobre los datos de un contacto.

--- tntconcept-core/src/main/java/com/autentia/tnt/tracking/hibernate/listener/TrackChangesUpdateListener.java
+++ tntconcept-core/src/main/java/com/autentia/tnt/tracking/hibernate/listener/TrackChangesUpdateListener.java
@@ -1,16 +1,16 @@
 /**
  * TNTConcept Easy Enterprise Management by Autentia Real Bussiness Solution S.L.
  * Copyright (C) 2007 Autentia Real Bussiness Solution S.L.
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,47 +20,34 @@ package com.autentia.tnt.tracking.hibernate.listener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.Session;
 import org.hibernate.event.PostUpdateEvent;
 import org.hibernate.event.PostUpdateEventListener;
-import org.hibernate.event.PreUpdateEvent;
-import org.hibernate.event.PreUpdateEventListener;
 import com.autentia.tnt.dao.ITransferObject;
 import com.autentia.tnt.tracking.Tracking;
 
 
-public class TrackChangesUpdateListener implements PostUpdateEventListener{
+public class TrackChangesUpdateListener implements PostUpdateEventListener {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8083135702492242171L;
-	private static final Log log = LogFactory.getLog(TrackChangesUpdateListener.class);
+    private static final long serialVersionUID = 8083135702492242171L;
 
-	public void onPostUpdate(PostUpdateEvent event) {
-		try {
-			
-			ITransferObject transferObject = (ITransferObject)event.getEntity();
-			
-			if (transferObject instanceof Tracking) {
-				Tracking tracking = (Tracking)transferObject;
-				tracking.commitCurrentChanges();
-				
-				tracking.notifyChanges();
-			}
-		} catch (SecurityException e) {
-			log.error(e);
-		} catch(ClassCastException e){
-			//lo ignoramos al no tratarse de una entidad bajo el control de cambios
-		}
-		
-	}
+    private static final Log log = LogFactory.getLog(TrackChangesUpdateListener.class);
 
+    public void onPostUpdate(PostUpdateEvent event) {
+        try {
 
+            ITransferObject transferObject = (ITransferObject) event.getEntity();
 
-	
+            if (transferObject instanceof Tracking) {
+                Tracking tracking = (Tracking) transferObject;
+                tracking.commitCurrentChanges();
 
+            }
+        } catch (SecurityException e) {
+            log.error(e);
+        } catch (ClassCastException e) {
+            //lo ignoramos al no tratarse de una entidad bajo el control de cambios
+        }
 
+    }
 
-	
 }
