be0988b9be3d4aae2e277333cb796b51ac787672
Javier Alonso <jalonso@autentia.com>
2017-09-06 12:22:00 +0200

    fix a bug displaying wrong monthly hours if a holiday request spans two months

---
+++ tntconcept-web/src/test/java/com/autentia/tnt/bean/activity/ActivityBean_IT.java
@@ -0,0 +1,167 @@
+package com.autentia.tnt.bean.activity;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.autentia.tnt.businessobject.Holiday;
+import com.autentia.tnt.businessobject.HolidayState;
+import com.autentia.tnt.businessobject.RequestHoliday;
+import com.autentia.tnt.businessobject.User;
+import com.autentia.tnt.manager.holiday.HolidayManager;
+import com.autentia.tnt.manager.holiday.RequestHolidayManager;
+import com.autentia.tnt.test.utils.SpringUtilsForTesting;
+import com.autentia.tnt.util.HibernateUtil;
+import com.autentia.tnt.util.SpringUtils;
+
+public class ActivityBean_IT {
+
+	private SessionFactory sessionFactory;
+	private User loggedUser;
+
+	@Before
+	public void setup() {
+		// test application context
+		ApplicationContext appCtx = new ClassPathXmlApplicationContext("applicationContext-test.xml");
+		SpringUtils.configureTest(appCtx);
+
+		// prepare hibernate
+		sessionFactory = HibernateUtil.getSessionFactory();
+		sessionFactory.openSession();
+		sessionFactory.getCurrentSession().beginTransaction();
+
+		// a principal is required in the security context
+		SpringUtilsForTesting.configure(appCtx);
+		loggedUser = SpringUtilsForTesting.createUserInContextWithRoleAndDepartment();
+	}
+
+	@After
+	public void rollback() {
+		sessionFactory.getCurrentSession().getTransaction().rollback();
+	}
+
+	@Test
+	public void getMonthTotalHours_HolidayRequestOverMonths() {
+
+		// create a holiday request over two months
+		RequestHolidayManager rhManager = RequestHolidayManager.getDefault();
+		RequestHoliday requestOverMonths = new RequestHoliday();
+		requestOverMonths.setUserRequest(loggedUser);
+		requestOverMonths.setState(HolidayState.ACCEPT);
+
+		LocalDate startRequest = LocalDate.of(2017, 8, 31);
+		Date startRequestDate = Date.from(startRequest.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		requestOverMonths.setBeginDate(startRequestDate);
+		LocalDate endRequest = LocalDate.of(2017, 9, 1);
+		Date endRequestDate = Date.from(endRequest.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		requestOverMonths.setFinalDate(endRequestDate);
+
+		rhManager.insertEntity(requestOverMonths);
+
+		// verify total hours for the first month
+		ActivityBeanNoJSF sut = new ActivityBeanNoJSF();
+		LocalDate august = LocalDate.of(2017, 8, 1);
+		Date augustDate = Date.from(august.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		sut.setSelectedDate(augustDate);
+		double augustHours = sut.getMonthTotalHours();
+		assertThat("First Month Hours", augustHours, closeTo(176.0, 0.1));
+
+		// verify total hours for the second month
+		LocalDate september = LocalDate.of(2017, 9, 1);
+		Date septemberDate = Date.from(september.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		sut.setSelectedDate(septemberDate);
+		double septHours = sut.getMonthTotalHours();
+		assertThat("Second Month Hours", septHours, closeTo(160.0, 0.1));
+	}
+
+	@Test
+	public void getMonthTotalHours_HolidayRequestWithinMonth() {
+
+		// create a holiday request within a month
+		RequestHolidayManager rhManager = RequestHolidayManager.getDefault();
+		RequestHoliday requestWithinMonth = new RequestHoliday();
+		requestWithinMonth.setUserRequest(loggedUser);
+		requestWithinMonth.setState(HolidayState.ACCEPT);
+
+		LocalDate startRequest = LocalDate.of(2017, 8, 25);
+		Date startRequestDate = Date.from(startRequest.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		requestWithinMonth.setBeginDate(startRequestDate);
+		LocalDate endRequest = LocalDate.of(2017, 8, 31);
+		Date endRequestDate = Date.from(endRequest.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		requestWithinMonth.setFinalDate(endRequestDate);
+
+		rhManager.insertEntity(requestWithinMonth);
+
+		// verify total hours for the month
+		ActivityBeanNoJSF sut = new ActivityBeanNoJSF();
+		LocalDate august = LocalDate.of(2017, 8, 1);
+		Date augustDate = Date.from(august.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		sut.setSelectedDate(augustDate);
+		double augustHours = sut.getMonthTotalHours();
+		assertThat(augustHours, closeTo(144.0, 0.1));
+	}
+
+	@Test
+	public void getMonthTotalHours_GeneralHolidaysInWorkingDays() {
+
+		// create a general holiday day
+		HolidayManager holidayManager = HolidayManager.getDefault();
+		Holiday holiday = new Holiday();
+		LocalDate holidayDay = LocalDate.of(2017, 8, 15);
+		Date holidayDate = Date.from(holidayDay .atStartOfDay(ZoneId.systemDefault()).toInstant());
+		holiday.setDate(holidayDate);
+		holidayManager.insertEntity(holiday);
+
+		// verify total hours for the month where the holiday day is in
+		ActivityBeanNoJSF sut = new ActivityBeanNoJSF();
+		LocalDate august = LocalDate.of(2017, 8, 1);
+		Date augustDate = Date.from(august.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		sut.setSelectedDate(augustDate);
+		double augustHours = sut.getMonthTotalHours();
+		assertThat(augustHours, closeTo(176.0, 0.1));
+	}
+
+	@Test
+	public void getMonthTotalHours_GeneralHolidaysInWeekend() {
+
+		// create a general holiday day in a weekend
+		HolidayManager holidayManager = HolidayManager.getDefault();
+		Holiday holiday = new Holiday();
+		LocalDate holidayInWeekendDay = LocalDate.of(2017, 8, 13);
+		Date holidayDate = Date.from(holidayInWeekendDay .atStartOfDay(ZoneId.systemDefault()).toInstant());
+		holiday.setDate(holidayDate);
+		holidayManager.insertEntity(holiday);
+
+		// verify total hours for the month where the holiday is in
+		ActivityBeanNoJSF sut = new ActivityBeanNoJSF();
+		LocalDate august = LocalDate.of(2017, 8, 1);
+		Date augustDate = Date.from(august.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		sut.setSelectedDate(augustDate);
+		double augustHours = sut.getMonthTotalHours();
+		assertThat(augustHours, closeTo(184.0, 0.1));
+	}
+
+	/**
+	 * Seam to avoid that JSF context is loaded for the ActivityBean managed bean
+	 *
+	 * @author jalonso
+	 *
+	 */
+	private class ActivityBeanNoJSF extends ActivityBean {
+		@Override
+		public float getHoursPerDay() {
+			return 8;
+		}
+	}
+
+}
