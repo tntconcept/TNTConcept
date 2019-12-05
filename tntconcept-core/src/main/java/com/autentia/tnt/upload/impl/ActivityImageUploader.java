591b0c1f1bb2a4c74d46348fe439ada9749ce0a1
urbanojvr <u.villanueva95@gmail.com>
2019-12-05 13:55:17 +0100

    Comprime imágenes de las imputaciones

--- tntconcept-core/src/main/java/com/autentia/tnt/upload/impl/ActivityImageUploader.java
+++ tntconcept-core/src/main/java/com/autentia/tnt/upload/impl/ActivityImageUploader.java
@@ -2,41 +2,54 @@ package com.autentia.tnt.upload.impl;
 
 import com.autentia.tnt.businessobject.Activity;
 import com.autentia.tnt.util.ConfigurationUtil;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.myfaces.custom.fileupload.UploadedFile;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 
 public class ActivityImageUploader {
 
-    private static final String EXTENSION = ".jpg";
+    private static final String EXTENSION = "jpg";
 
     public static boolean store(UploadedFile file, Activity activity) {
         String fileName = generateFileName(activity.getInsertDate(),activity.getId());
         File destinationFile = new File(fileName);
 
-        InputStream in = null;
         OutputStream out = null;
-        byte[] buffer = new byte[65536];
-        int nr;
+        ImageOutputStream ios = null;
+        BufferedImage bufferedImage;
 
         try {
-            in = file.getInputStream();
-            destinationFile.getParentFile().mkdirs();
+            bufferedImage = ImageIO.read(file.getInputStream());
             out = new FileOutputStream(fileName);
+            destinationFile.getParentFile().mkdirs();
 
-            while ((nr = in.read(buffer)) != -1) {
-                out.write(buffer, 0, nr);
-            }
+            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(EXTENSION);
+            ImageWriter writer = writers.next();
+
+            ios = ImageIO.createImageOutputStream(out);
+            writer.setOutput(ios);
+
+            ImageWriteParam param = writer.getDefaultWriteParam();
+
+            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            param.setCompressionQuality(0.3f);
+            writer.write(null, new IIOImage(bufferedImage, null, null), param);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         } finally {
-            if (in != null) {
+            if (ios != null) {
                 try {
-                    in.close();
+                    ios.close();
                 } catch (IOException e) {
                     System.err.println(e.getMessage());
                 }
@@ -71,7 +84,7 @@ public class ActivityImageUploader {
         String fileName = getBasePath() +
                 calendar.get(Calendar.YEAR) + "/" +
                 (calendar.get(Calendar.MONTH) + 1) + "/" +
-                id + EXTENSION;
+                id + "." + EXTENSION;
 
         return fileName;
     }
@@ -83,7 +96,7 @@ public class ActivityImageUploader {
         String fileName = getBasePath() +
                 calendar.get(Calendar.YEAR) + "/" +
                 (calendar.get(Calendar.MONTH) + 1) + "/" +
-                name + EXTENSION;
+                name + "." + EXTENSION;
 
         return fileName;
     }
