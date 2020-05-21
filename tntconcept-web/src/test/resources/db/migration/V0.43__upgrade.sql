c2993c0ead402a83e3c2e4c5be1b17f81b426ea0
Daniel Otero <dotero@autentia.com>
2020-05-21 11:42:19 +0200

    Create extra user for testing

--- tntconcept-web/src/test/resources/db/migration/V0.43__upgrade.sql
+++ tntconcept-web/src/test/resources/db/migration/V0.43__upgrade.sql
@@ -1,4 +1,7 @@
 
+insert into User (name, login, password, roleId, categoryId, startDate, workingInClient, married, childrenNumber, email)
+values ('User', 'user', 'dd94709528bb1c83d08f3088d4043f4742891f4f', 1, 1, now(), 0, 0, 0, 'user@user.com');
+
 -- -----------------------------------------------------------------------------
 -- ProjectRole
 -- -----------------------------------------------------------------------------
