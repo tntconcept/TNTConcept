66f21f6162df1cb28a08e13e4e1186fa5a3dfbfe
Francisco Javier Martínez Páez <fjmpaez@autentia.com>
2022-01-27 06:59:39 +0100

    Create script for next 0.45 version including AnnualWorkSummary tables

---
+++ tntconcept-core/src/main/resources/com/autentia/tnt/sql/mysql/0.44/upgrade.sql
@@ -0,0 +1,33 @@
+-- -----------------------------------------------------------------------------
+-- Version
+-- -----------------------------------------------------------------------------
+--
+
+-- Set max default hours to work to 1765 hours in minutes
+UPDATE WorkingAgreement
+SET yearDuration = 105900;
+
+CREATE TABLE AnnualWorkSummary
+(
+    userId            INT(10) NOT NULL,
+    year              INT (4) NOT NULL,
+    earnedVacations   INT (2) NOT NULL,
+    acceptedVacations INT (2) NOT NULL,
+    targetHours       DECIMAL(7, 2) NOT NULL,
+    workedHours       DECIMAL(7, 2) NOT NULL,
+    insertDate        DATETIME DEFAULT NULL,
+    updateDate        DATETIME DEFAULT NULL,
+    PRIMARY KEY (userId, year),
+    CONSTRAINT fk_annualworksummary_user FOREIGN KEY (userId) REFERENCES User (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+CREATE TABLE AnnualWorkSummaryJob
+(
+    `id`          INT      NOT NULL AUTO_INCREMENT,
+    lastExecution DATETIME NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- Update version number
+UPDATE Version
+SET version='0.45';
