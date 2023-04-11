39a60a1cc93c9073bfa0d94d4268a7877e4d0046
Cristian.bahamonde <123061822+cristianbahamondesantamaria@users.noreply.github.com>
2023-04-11 11:24:37 +0200

    Refactor install_dependencies.sh and remove duplicates (#102)
    * Refactor install_dependencies.sh and remove duplicates
    
    * untab lines

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,31 +1,11 @@
 #!/usr/bin/env bash
-
-function copy(){
-	filePath=$1
-	newPath=$2
-
-	if [ -f $filePath ];then
-		if ! [ -d $newPath ];then
-			newPathDir=$(echo $newPath | awk '{ sub("/[^/]*$",""); print}')
-			mkdir -p $newPathDir
-		fi
-		cp $filePath $newPath
-	else
-		echo "[!] Hubo un error al copiar $filePath a la ruta $newPath."
-	fi
-}
-echo "[+] Copying to Maven repository tntconcept-core dependencies..."
-copy tntconcept-core/jars_to_install/jta-1.0.1B.jar ~/.m2/repository/javax/transaction/jta/1.0.1B/jta-1.0.1B.jar
-
-echo "[+] Copying to Maven repository tntconcept-web dependencies..."
-copy tntconcept-web/jars_to_install/olap4j-0.9.6.jar ~/.m2/repository/org/olap4j/olap4j/0.9.6/olap4j-0.9.6.jar
-copy tntconcept-web/jars_to_install/mondrian-3.0.0.jar ~/.m2/repository/mondrian/mondrian/3.0.0/mondrian-3.0.0.jar
-copy tntconcept-web/jars_to_install/jpivot-1.8.0.jar ~/.m2/repository/com/tonbeller/jpivot/jpivot/1.8.0/jpivot-1.8.0.jar
-copy tntconcept-web/jars_to_install/javacup-0.10k.jar ~/.m2/repository/java_cup/javacup/0.10k/javacup-0.10k.jar
-copy tntconcept-web/jars_to_install/wcf-1.7.0.jar ~/.m2/repository/com/tonbeller/wcf/wcf/1.7.0/wcf-1.7.0.jar
-copy tntconcept-web/jars_to_install/tbutils-wcf-1.7.0.jar ~/.m2/repository/com/tonbeller/tbutils/tbutils-wcf/1.7.0/tbutils-wcf-1.7.0.jar
-copy tntconcept-web/jars_to_install/eigenbase-xom-1.0.jar ~/.m2/repository/org/eigenbase/xom/eigenbase-xom/1.0/eigenbase-xom-1.0.jar
-copy tntconcept-web/jars_to_install/eigenbase-properties-1.0.jar ~/.m2/repository/org/eigenbase/util/property/eigenbase-properties/1.0/eigenbase-properties-1.0.jar
-copy tntconcept-web/jars_to_install/eigenbase-resgen-1.0.jar ~/.m2/repository/org/eigenbase/resgen/eigenbase-resgen/1.0/eigenbase-resgen-1.0.jar
-
-echo "All dependencies copied!"
+./mvnw install:install-file -Dfile=./tntconcept-core/jars_to_install/jta-1.0.1B.jar -DgroupId=javax.transaction -DartifactId=jta -Dversion=1.0.1B -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/olap4j-0.9.6.jar -DgroupId=org.olap4j -DartifactId=olap4j -Dversion=0.9.6 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/mondrian-3.0.0.jar -DgroupId=mondrian -DartifactId=mondrian -Dversion=3.0.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/wcf-1.7.0.jar -DgroupId=com.tonbeller.wcf -DartifactId=wcf -Dversion=1.7.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/jpivot-1.8.0.jar -DgroupId=com.tonbeller.jpivot -DartifactId=jpivot -Dversion=1.8.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/tbutils-wcf-1.7.0.jar -DgroupId=com.tonbeller.tbutils -DartifactId=tbutils-wcf -Dversion=1.7.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/javacup-0.10k.jar -DgroupId=java_cup -DartifactId=javacup -Dversion=0.10k -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/eigenbase-properties-1.0.jar -DgroupId=org.eigenbase.util.property -DartifactId=eigenbase-properties -Dversion=1.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/eigenbase-resgen-1.0.jar -DgroupId=org.eigenbase.resgen -DartifactId=eigenbase-resgen -Dversion=1.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./tntconcept-web/jars_to_install/eigenbase-xom-1.0.jar -DgroupId=org.eigenbase.xom -DartifactId=eigenbase-xom -Dversion=1.0 -Dpackaging=jar
