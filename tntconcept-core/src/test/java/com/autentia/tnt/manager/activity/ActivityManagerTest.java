7756d69047bf369786ab3a1be77486d74d2559f6
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-05-23 12:12:15 +0200

    feat: check isWorkingTime project role flag instead of notWorkableProjectIds property

--- tntconcept-core/src/test/java/com/autentia/tnt/manager/activity/ActivityManagerTest.java
+++ tntconcept-core/src/test/java/com/autentia/tnt/manager/activity/ActivityManagerTest.java
@@ -1,57 +1,37 @@
 package com.autentia.tnt.manager.activity;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
-
+import com.autentia.tnt.businessobject.Activity;
+import com.autentia.tnt.businessobject.ProjectRole;
+import com.autentia.tnt.dao.hibernate.ActivityDAO;
+import com.autentia.tnt.util.ConfigurationUtil;
+import com.autentia.tnt.util.SpringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
-import com.autentia.tnt.businessobject.Activity;
-import com.autentia.tnt.businessobject.Project;
-import com.autentia.tnt.businessobject.ProjectRole;
-import com.autentia.tnt.dao.hibernate.ActivityDAO;
-import com.autentia.tnt.util.ConfigurationUtil;
-import com.autentia.tnt.util.SpringUtils;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ActivityManagerTest{
-
-    private static final Integer WORKING_PROJECT = 1;
-    private static final Integer NOT_WORKING_PROJECT_1 = 2;
-    private static final Integer NOT_WORKING_PROJECT_2 = 2;
-
     private final ConfigurationUtil configurationUtil = mock(ConfigurationUtil.class);
-    ActivityManager activityManager = new ActivityManager(mock(ActivityDAO.class), configurationUtil);
+    ActivityManager activityManager = new ActivityManager(mock(ActivityDAO.class));
     final ProjectRole workingRoleProject1 = mock(ProjectRole.class);
     final ProjectRole workingRoleProject2 = mock(ProjectRole.class);
     final ProjectRole notWorkingRoleProject1 = mock(ProjectRole.class);
     final ProjectRole notWorkingRoleProject2 = mock(ProjectRole.class);
 
-    final Project workingProject = mock(Project.class);
-    final Project notWorkingProject1 = mock(Project.class);
-    final Project notWorkingProject2 = mock(Project.class);
-
     final static ApplicationContext ctx = mock(ApplicationContext.class);
 
     @Before
     public void init(){
-        when(workingProject.getId()).thenReturn(WORKING_PROJECT);
-        when(notWorkingProject1.getId()).thenReturn(NOT_WORKING_PROJECT_1);
-        when(notWorkingProject2.getId()).thenReturn(NOT_WORKING_PROJECT_2);
-
-        when(workingRoleProject1.getProject()).thenReturn(workingProject);
-        when(workingRoleProject2.getProject()).thenReturn(workingProject);
-        when(notWorkingRoleProject1.getProject()).thenReturn(notWorkingProject1);
-        when(notWorkingRoleProject2.getProject()).thenReturn(notWorkingProject2);
-
-        when(configurationUtil.getNotWorkingTimeProjectIds()).thenReturn(
-                Arrays.asList(NOT_WORKING_PROJECT_1, NOT_WORKING_PROJECT_2));
-
-        when(ctx.getBean("configuration")).thenReturn(configurationUtil);
+        when(workingRoleProject1.getIsWorkingTime()).thenReturn(true);
+        when(workingRoleProject2.getIsWorkingTime()).thenReturn(true);
+        when(notWorkingRoleProject1.getIsWorkingTime()).thenReturn(false);
+        when(notWorkingRoleProject2.getIsWorkingTime()).thenReturn(false);
         SpringUtils.configureTest(ctx);
 
     }
