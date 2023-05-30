c751b337c24c6f971722f723e378352c9be3b1c2
Gabriel Costanza <costanzag1999@gmail.com>
2023-05-30 11:11:32 +0200

    chore: user dao integration test

--- tntconcept-core/src/test/java/com/autentia/tnt/test/utils/SpringUtilsForTesting.java
+++ tntconcept-core/src/test/java/com/autentia/tnt/test/utils/SpringUtilsForTesting.java
@@ -22,8 +22,19 @@
 
 package com.autentia.tnt.test.utils;
 
+import com.autentia.tnt.businessobject.*;
+import com.autentia.tnt.dao.hibernate.DepartmentDAO;
+import com.autentia.tnt.dao.hibernate.RoleDAO;
+import com.autentia.tnt.dao.hibernate.UserCategoryDAO;
+import com.autentia.tnt.manager.security.AuthenticationManager;
+import com.autentia.tnt.manager.security.Principal;
 import com.autentia.tnt.util.SpringUtils;
+import org.acegisecurity.Authentication;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
 
 public class SpringUtilsForTesting {
 
@@ -43,5 +54,90 @@ public class SpringUtilsForTesting {
         return appCtx.getBean(name);
     }
 
+    public static void loadPrincipalInSecurityContext(String username) {
+        AuthenticationManager authManager = (AuthenticationManager) getSpringBean("userDetailsService");
+        Principal principal = (Principal) authManager.loadUserByUsername(username);
+        Authentication auth = new UsernamePasswordAuthenticationToken(principal, principal.getUser().getPassword(),
+                principal.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    public static User createUser(String login) {
+        final User user = new User();
+        user.setLogin(login);
+        user.setPassword(login);
+        user.setName(login);
+        user.setRole(createRoleInContext());
+        user.setDepartment(createDepartmentInContext());
+        user.setCategory(createUserCategoryInContext());
+        user.setAgreement(createAgreementInContext());
+        user.setActive(true);
+        user.setStartDate(new java.util.Date());
+        user.setNif("");
+        user.setAcademicQualification("");
+        user.setPhone("");
+        user.setMobile("");
+        user.setStreet("");
+        user.setCity("");
+        user.setPostalCode("");
+        user.setMarried(true);
+        user.setChildrenNumber(0);
+        user.setDrivenLicenseType("");
+        user.setLicensePlate("");
+        user.setSocialSecurityNumber("");
+        user.setBank("");
+        user.setAccount("");
+        user.setTravelAvailability("");
+        user.setWorkingInClient(true);
+        user.setEmail("");
+        user.setGenre(UserGenre.MAN);
+        user.setSalary(BigDecimal.ZERO);
+        user.setSalaryExtras(BigDecimal.ZERO);
+        user.setSecurityCard("");
+        user.setHealthInsurance("");
+        user.setNotes("");
+        user.setPhoto("");
+        user.setContractObservations("");
+        user.setDayDuration(8);
+        user.setVehicleType("");
+        return user;
+    }
+
+    private static WorkingAgreement createAgreementInContext() {
+
+        final WorkingAgreement agreement = new WorkingAgreement();
+        agreement.setName("Test Agreement");
+        agreement.setDescription("");
+        agreement.setId(1);
+        return agreement;
+    }
+
+    private static UserCategory createUserCategoryInContext() {
+
+        final UserCategory category = new UserCategory();
+        category.setName("Test Category");
+        final UserCategoryDAO userCategoryDAO = (UserCategoryDAO) appCtx.getBean("daoUserCategory");
+        userCategoryDAO.insert(category);
+        return category;
+    }
+
+    private static Department createDepartmentInContext() {
+
+        final Department department = new Department();
+        department.setName("Test Department");
+        department.setDescription("Test Department");
+        final DepartmentDAO departmentDao = (DepartmentDAO) appCtx.getBean("daoDepartment");
+        departmentDao.insert(department);
+        return department;
+    }
+
+    private static Role createRoleInContext() {
+
+        final Role role = new Role();
+        role.setName("Test Role");
+        final RoleDAO roleDao = (RoleDAO) appCtx.getBean("daoRole");
+        roleDao.insert(role);
+        return role;
+    }
 
 }
