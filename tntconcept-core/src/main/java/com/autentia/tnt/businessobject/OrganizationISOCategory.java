fb31c7a6f0c613ee5c5af3fdef5ec070e238500d
Gabriel Costanza <costanzag1999@gmail.com>
2023-06-01 16:32:56 +0200

    chore: organizationDAO integration test

--- tntconcept-core/src/main/java/com/autentia/tnt/businessobject/OrganizationISOCategory.java
+++ tntconcept-core/src/main/java/com/autentia/tnt/businessobject/OrganizationISOCategory.java
@@ -75,7 +75,7 @@ public class OrganizationISOCategory implements Serializable, ITransferObject
   public Integer getId() {
     return id;
   }
-  private void setId( Integer id ) {
+  protected void setId( Integer id ) {
     this.id = id;
   }
       
