dacdb47b2ff868846c93e4170594c0ca103e5610
Francisco Javier Martínez Páez <fjmpaez@Mac-Book-Pro-fjmpaez.local>
2024-09-26 17:15:50 +0200

    Doc Servlet can download evidences with same name but different extension

--- tntconcept-web/src/main/java/com/autentia/tnt/servlet/DocServlet.java
+++ tntconcept-web/src/main/java/com/autentia/tnt/servlet/DocServlet.java
@@ -1,122 +1,92 @@
 /**
  * TNTConcept Easy Enterprise Management by Autentia Real Bussiness Solution S.L.
  * Copyright (C) 2007 Autentia Real Bussiness Solution S.L.
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
- * 
+ *
  */
 package com.autentia.tnt.servlet;
 
+import com.autentia.tnt.util.ConfigurationUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.*;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-import org.apache.commons.logging.*;
-
-import com.autentia.tnt.util.*;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author ivan
- *
  */
 public class DocServlet extends HttpServlet {
 
-	/** */
-	private static final long serialVersionUID = -6588318861276755992L;
-	
-	/** Logger */
-	private static final Log log = LogFactory.getLog(DocServlet.class);
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6588318861276755992L;
 
-	public static final String ARG_MIME = "mime";
-	public static final String URL_PREFIX = "/doc/";
-	
-	@Override
-	protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException 
-	{
+    /**
+     * Logger
+     */
+    private static final Log log = LogFactory.getLog(DocServlet.class);
 
-		String uri = request.getRequestURI();
-		log.debug("doGet - uri='"+uri+"'");
-		
-		int i = uri.indexOf(URL_PREFIX);
-		if( i!=-1 )
-		{
-			String relPath = uri.substring( i+URL_PREFIX.length() );
-			relPath = URLDecoder.decode(relPath,"UTF-8");
-			log.debug("doGet - relPath='"+relPath+"'");
-			
-			File f = new File( ConfigurationUtil.getDefault().getUploadPath()+relPath );
-			if( f.exists() )
-			{
-				response.setContentLength((int) f.length());
-			
-				String mime = request.getParameter(ARG_MIME);
-				if (mime!=null && !mime.equals("")) {
-					response.setContentType(mime);
-				}
-				OutputStream out = response.getOutputStream();
-				InputStream in = null;
-				try {
-					in = new FileInputStream(f);
-					byte[] buffer = new byte[8192];
-					int nr;
-					while( (nr=in.read(buffer))!=-1 ){
-						out.write(buffer,0,nr);
-					}
-				} finally {
-					if(in!=null){
-						in.close();
-					}
-				}
-			}
-			else
-			{
-				response.sendError( HttpServletResponse.SC_NOT_FOUND );
-			}
-		}
-		else
-		{
-			response.sendError( HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
-									"Bad URL prefix for servlet: check your web.xml file" );
-		}
-	}
+    public static final String ARG_MIME = "mime";
+    public static final String URL_PREFIX = "/doc/";
 
-	/*
-	@Override
-	protected long getLastModified(HttpServletRequest request) 
-	{
-		String uri = request.getRequestURI();
-		int i = uri.indexOf(URL_PREFIX);
-		if( i!=-1 )
-		{
-			String relPath = uri.substring( i+URL_PREFIX.length() );
-			File f = new File( ConfigurationUtil.getUploadPath()+relPath );
-			return f.lastModified();
-		}
-		return 0;
-	}
-	*/
+
+    private String removeServletPrefixFromPath(String requestedUri) throws UnsupportedEncodingException {
+        String relPath = requestedUri.substring(requestedUri.indexOf(URL_PREFIX) + URL_PREFIX.length());
+        return URLDecoder.decode(relPath, StandardCharsets.UTF_8);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String requestURI = request.getRequestURI();
+        String relPath = removeServletPrefixFromPath(requestURI);
+
+        log.debug("doGet - relPath='" + relPath + "'");
+
+        DocumentFinder.RequestedDocumentFile requestedDocumentFile = DocumentFinder.getDocumentFinder().search(ConfigurationUtil.getDefault().getUploadPath() + relPath);
+
+        if (requestedDocumentFile.requestedFile().exists()) {
+            if (requestedDocumentFile.original()) {
+                writeFile(request, response, requestedDocumentFile.requestedFile());
+            } else {
+                final String uriParent = requestURI.substring(0, requestURI.lastIndexOf("/"));
+                response.sendRedirect(uriParent + "/" + requestedDocumentFile.requestedFile().getName());
+            }
+
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private static void writeFile(HttpServletRequest request, HttpServletResponse response, File theRequestedFile) throws IOException {
+        response.setContentLength((int) theRequestedFile.length());
+        String mime = request.getParameter(ARG_MIME);
+        if (mime != null && !mime.isEmpty()) {
+            response.setContentType(mime);
+        }
+        FileUtils.copyFile(theRequestedFile, response.getOutputStream());
+    }
+
 }
-
-
-
-
-
-
-
-
-
